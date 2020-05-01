bb25716b4adfccdfaefd1d92e95e3ac95db23bac
smhassan123 <smhassan.pervez@gmail.com>
2020-05-01 21:23:38 +0500

    Chap 9

--- car.py
+++ car.py
@@ -22,3 +22,18 @@ class ModCar():
     
     def incrementOdometer(self, miles):
         self.odoMeterReading += miles
+
+
+class ModBattery():
+    def __init__(self, batterySize=70):
+        self.battery = batterySize
+        
+    def describeBattery(self):
+        print(f"This car has {self.battery}-kWh battery")
+    
+        
+
+class ModElectricCar(ModCar):
+    def __init__(self, make, model, year):
+        super().__init__(make, model, year)
+        self.batt = ModBattery()
